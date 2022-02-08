5d2b67275d3e35d5f1c12dabf5fb2f7728b31ca8
Stefano Mersi <stefano.mersi@cern.ch>
2022-02-08 14:09:05 +0100

    Creating my directories

--- sendAll.py
+++ sendAll.py
@@ -8,13 +8,20 @@ smtpPort=25
 smtpHost="cernmx.cern.ch"
 
 path = './'
-myDir="exampledir/"
-listing = os.listdir(myDir)
+spoolDir="spool/"
+sentDir="sent/"
+
+dirMode = 0o700
+
+if (not os.path.exists(spoolDir)) : os.mkdir(spoolDir, dirMode)
+if (not os.path.exists(sentDir)) : os.mkdir(sentDir, dirMode)
+
+listing = os.listdir(spoolDir)
 
 s = smtplib.SMTP(host=smtpHost, port=smtpPort)
 
 for fle in listing:
-  full_path = os.path.join(myDir, fle)
+  full_path = os.path.join(spoolDir, fle)
   msg = email.message_from_file(open(full_path))
   try:
     s.send_message(msg)
