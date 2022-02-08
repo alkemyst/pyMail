bfa8d8b519c12cae8eeaf6f77d96105a12515e1d
Stefano Mersi <stefano.mersi@cern.ch>
2022-02-08 14:47:17 +0100

    Adding a convenient logfile

--- sendAll.py
+++ sendAll.py
@@ -3,22 +3,29 @@
 import email
 import os
 import smtplib
+from datetime import datetime
 
 smtpPort=25
 smtpHost="cernmx.cern.ch"
 
 path = './'
-spoolDir="spool/"
-sentDir="sent/"
+spoolDir="Spool/"
+sentDir="Sent/"
+logFile="log.txt"
 
 dirMode = 0o700
 
 if (not os.path.exists(spoolDir)) : os.mkdir(spoolDir, dirMode)
 if (not os.path.exists(sentDir)) : os.mkdir(sentDir, dirMode)
 
-listing = os.listdir(spoolDir)
+dateTimeObj = datetime.now()
+myLog = open(logFile, 'a+')
+myLog.write("Send invoked at " + dateTimeObj.strftime("%Y-%b-%d %H:%M:%S") + " : " );
 
+listing = os.listdir(spoolDir)
 s = smtplib.SMTP(host=smtpHost, port=smtpPort)
+
+nMail=0
 
 for emlFile in listing:
   spool_path = os.path.join(spoolDir, emlFile)
@@ -29,7 +36,10 @@ for emlFile in listing:
   except smtplib.SMTPException as e:
     print(e)
   del msg
+  nMail=nMail+1
   os.replace(spool_path, sent_path)
 
 
 
+myLog.write("%d email(s) sent\n" % nMail);
+myLog.close()
